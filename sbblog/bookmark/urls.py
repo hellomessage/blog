c57533ed10741322891cde0b5782d96fa42a0545
message0316 <message0316@gmail.com>
2016-10-05 11:57:03 +0900

    blog coding

---
+++ sbblog/bookmark/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from bookmark.models import Bookmark
+from bookmark.views import BookmarkLV, BookmarkDV
+
+urlpatterns = [
+    # Class-base views
+    url(r'^$', BookmarkLV.as_view(model = Bookmark), name='index'),
+    url(r'^(?P<pk>\d+)/$', BookmarkDV.as_view(model = Bookmark), name='detail'),
+]
